0f2b4d582a2f54e02ffd8cae81b8768a2be4d874
AkshayViswanathan <akshayviswanathan@techversantinfotech.com>
2024-03-01 18:22:00 +0530

    update

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="container">
-{{ date }}
-<p>Location: {{ datafetch?.name }}</p>
-<p>{{ datafetch?.main?.temp }}</p>
+      <div class="flex gap-20px" >
+           <div > 
+              <p>{{ datafetch?.main?.temp }}</p>
+                 </div> 
+                  <div>
+                <p>Location: {{ datafetch?.name }}</p>
+             <p>{{ date }} </p> 
+         </div>
+   
+        </div>
+   
 
   </div>
 
@@ -15,14 +23,14 @@
       <p>{{ datafetch }}</p>
       
 
-      <div class="flex justify-around">
+      <div class="flex justify-around" >
           <p> {{ datafetch?.weather[0]?.main }}</p>
           <label > {{ datafetch?.clouds?.all }}% </label>
       </div>
       
       <div class="flex justify-around">
          <label >Temp_min</label>
-         <p>{{ Celcius }}</p>
+         <p>{{ celciusmintemp }}</p>
       </div>
        
       <div class="flex justify-around">
@@ -42,9 +50,10 @@
 import { ref } from "vue";
 import axios from "axios";
 
+
 const datafetch = ref(null)
 const searchTerm = ref(null)
-// const clear= ref(null)
+const celciusmintemp = ref(null)
 
 const date = new Date().toLocaleDateString('en-us', {
   weekdays: 'long',
@@ -55,7 +64,7 @@ const date = new Date().toLocaleDateString('en-us', {
 })
 
 
-const Celcius = (datafetch?.value?.main?.temp_min -32) / 1.8;
+
 
 
 // const handleSearch= ()=>{
@@ -76,13 +85,20 @@ async function handleSubmit(){
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchTerm.value}&appid=9a07fd6e72eba80e9494d4624edf23f9`;
   const data = await axios.get(apiUrl).then(response => response.data)
   datafetch.value = data
-console.log(datafetch);}
+console.log(datafetch)
+calculation()
+}
 catch(error){ 
   console.error("fetch faild" )
+ 
 }
 }
 
-
+function calculation(){
+  const Celcius = (datafetch?.value?.main?.temp_min - 32) * 5/9
+  celciusmintemp.value= Celcius
+console.log(Celcius);
+}
 </script>
 
 <style  scoped>
