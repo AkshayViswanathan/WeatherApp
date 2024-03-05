8709056d8e6a4862ea82856c01384ef41c54b831
AkshayViswanathan <akshayviswanathan@techversantinfotech.com>
2024-03-05 12:17:18 +0530

    updated

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,66 @@
 <template>
   
-      <div class="max-h-screen" >
-          <div  >
-            <div class="max-h-screen ">
-              <img v-if="datafetch?.weather[0]?.main === 'Clouds'" :src="clouds" alt="" class="bg-cover bg-center 	" />
-          <img v-else-if="datafetch?.weather[0]?.main === 'Rain'" :src="rain" alt="" class="max-h-screen	" />
-          <img v-else-if="datafetch?.weather[0]?.main === 'Clear'" :src="clear" alt="" class="max-h-screen" />
-          <img v-else-if="datafetch?.weather[0]?.main === 'Mist'" :src="mist" alt="" class="max-h-screen" />
-          <img v-else-if="datafetch?.weather[0]?.main === 'Haze'" :src="haze" alt="" class="max-h-screen" />
-          <img v-else-if="datafetch?.weather[0]?.main === 'Snow'" :src="snow" alt="" class="max-h-screen" />
-          <img v-else :src="defaultimage" alt="" class="bg-contain	" />
+      <div class="h-screen overflow-hidden" >
+          <div class="h-screen overflow-hidden" >
+            <div class="h-screen w-full ">
+               <img v-if="datafetch?.weather[0]?.main === 'Clouds'" :src="clouds" alt="" class="w-[100%] h-full object-cover" />
+          <img v-else-if="datafetch?.weather[0]?.main === 'Rain'" :src="rain" alt="" class="w-[100%] h-full object-cover" />
+          <img v-else-if="datafetch?.weather[0]?.main === 'Clear'" :src="clear" alt="" class="w-[100%] h-full object-cover	" />
+          <img v-else-if="datafetch?.weather[0]?.main === 'Mist'" :src="mist" alt="" class="w-[100%] h-full object-cover	" />
+          <img v-else-if="datafetch?.weather[0]?.main === 'Haze'" :src="haze" alt=""  class="w-[100%] h-full object-cover	" />
+          <img v-else-if="datafetch?.weather[0]?.main === 'Snow'" :src="snow" alt="" class="w-[100%] h-full object-cover	" />
+          <img v-else :src="defaultimage" alt="" class="w-full h-full object-cover" />
             </div>
             <div class="absolute top-[100px] left-[100px] z-4">
-              <p class="text-black ">{{ rounoffDegreeCelcus }} °C</p>
-                <p class="text-red-500">Location: {{ datafetch?.name }}, {{ datafetch?.sys?.country }}</p>
-                <p>{{ Todysdate }} </p> 
+              <p class="text-black text-6xl text-white">{{ rounoffDegreeCelcus }} °C</p>
+              <p class="text-white	text-5xl "> {{ datafetch?.weather[0]?.main }}</p>
+                <p class="text-red-500 text-4xl">Location: {{ datafetch?.name }}, {{ datafetch?.sys?.country }}</p>
+                <p class="text-3xl text-white">{{ Todysdate }} </p> 
 
             </div>
            </div>
   
    
-  <div class="absolute top-0 right-0 w-[40%] h-[60vh] bg-slate-800 p-8 bg-gray-500	">
+  <div class="glassmorphism absolute top-0 right-0 w-[40%] h-[100vh] bg-slate-800 p-8 bg-gray-500	">
      
     
      <input class="seachbox rounded-md items-starts my-8 ml-8 mr-2 p-2"  type="text" v-model="searchTerm" placeholder="search..." />
      <i class="fa-solid fa-magnifying-glass text-white	hover:bg-gray-600 text-xl" @click="handleSubmit"></i>
+       <div v-if="datafetch" class="flex flex-col gap-8 mx-10">
 
+  
      <p class="text-white align-center">Weather Details...</p>
+     <p class=" text-white uppercase">{{ datafetch?.weather[0]?.description }}</p>
 
 
-     <div class="flex justify-around">
+     <div class="flex justify-between">
         <label class="text-white	" >Temp_min</label>
-        <p class="text-white	">{{ datafetch?.main?.temp_min }}</p>
+        <p class="text-white">{{ datafetch?.main?.temp_min }}</p>
      </div>
       
-     <div class="flex justify-around">
+     <div class="flex justify-between">
           <label for="tempMax" class="text-white	">Temp_max</label>
           <p class="text-white	">{{ datafetch?.main?.temp_max }}</p>
       </div>
 
-     <div class="flex justify-around" >
-         <p class="text-white	"> {{ datafetch?.weather[0]?.main }}</p>
+     <div class="flex justify-between" >
+         <p class="text-white	">clouds</p>
          <label class="text-white	"> {{ datafetch?.clouds?.all }}% </label>
      </div>
      
-      <div class="flex justify-around">
+      <div class="flex justify-between">
         <label class="text-white	" >Humidity</label>
         <p class="text-white	">{{ datafetch?.main?.humidity}}</p>
       </div >
-      <div class="flex 	justify-around">
+      <div class="flex 	justify-between">
         <label class="text-white	" > Wind </label>
         <p class="text-white	"> {{ datafetch?.wind?.speed}} </p>
 
       </div>
-    
+         </div>
  </div>
   </div>
-  <p>{{ datafetch }}</p>
 
-  
 </template>
 
 
@@ -71,13 +73,16 @@ import mist from "./assets/mist.jpg"
 import snow from "./assets/snow.jpeg"
 import rain from "./assets/rain2.jpg" 
 import clear from "./assets/clear.jpg" 
-import clouds from "./assets/Clouds.jpg"
-import defaultimage from "./assets/default.jpg"
+import clouds from "./assets/clouds.jpg"
+import haze from "./assets/haze.jpg"
+import defaultimage from "./assets/default2.jpg"
 
 
 const datafetch = ref(null)
 const searchTerm = ref(null)
 const rounoffDegreeCelcus = ref(null)
+const APIKEY =import.meta.env.VITE_API_KEY;
+const APIURL =import.meta.env.VITE_API_URL ;
 
 
 const Todysdate = new Date().toLocaleDateString('en-us', {
@@ -92,14 +97,12 @@ const Todysdate = new Date().toLocaleDateString('en-us', {
 
 async function handleSubmit(){
   try{
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchTerm.value}&units=metric&appid=9a07fd6e72eba80e9494d4624edf23f9`;
+  const apiUrl = `${APIURL}?q=${searchTerm.value}&units=metric&appid=${APIKEY}`;
   const data = await axios.get(apiUrl).then(response => response.data)
   datafetch.value = data
-
 }
 catch(error){ 
   console.error("fetch faild" )
- 
 }
 }
 
